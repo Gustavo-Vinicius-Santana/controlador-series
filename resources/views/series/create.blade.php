0f3d2d36182828110776a9916330f88e30314577
Gustavo_Vinicius_SX <gus.tec.dev@gmail.com>
2024-04-30 16:00:06 -0300

    configuração do front-end de cadastro

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,6 +1,26 @@
 <x-layout title="Nova serie">
-    <div class="container custom-container w-50 mx-auto">
-        <x-series.form action="{{ route('series.store')}}" nome="{{ old('nome') }}"/>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <form action="{{ route('series.store')}}" method="post">
+                @csrf
+                <div class="mb-3 text-center">
+                    <label for="nome" class="form-label">Nome</label>
+                    <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite o nome da série" value="{{ old('nome') }}">
+                </div>
+                <div class="row">
+                    <div class="col-md-6 mb-3 text-center">
+                        <label for="seasonQty" class="form-label">Temporadas</label>
+                        <input type="text" class="form-control" id="seasonQty" name="seasonQty" placeholder="Digite o número de temporadas" value="{{ old('seasonQty') }}">
+                    </div>
+                    <div class="col-md-6 mb-3 text-center">
+                        <label for="episodesPerSeason" class="form-label">Episódios</label>
+                        <input type="text" class="form-control" id="episodesPerSeason" name="episodesPerSeason" placeholder="Digite o número de episódios" value="{{ old('episodesPerSeason') }}">
+                    </div>
+                </div>
+                <div class="text-center">
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                </div>
+            </form>
+        </div>
     </div>
-
 </x-layout>
--- resources/views/series/edit.blade.php
+++ resources/views/series/edit.blade.php
@@ -1,4 +1,4 @@
-<x-layout title="Editar serie {{ $series->nome }}">
+<x-layout title="Editar serie {!! $series->nome !!}">
     <div class="container custom-container w-50 mx-auto">
         <x-series.form action="{{ route('series.update', $series) }}"
         nome="{{ $series->nome }}" id="{{ $series->id }}"/>
