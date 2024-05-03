1b29dd24b6332bc77d0f4dac57136daff9d66cd9
Gustavo_Vinicius_SX <gus.tec.dev@gmail.com>
2024-05-03 10:52:42 -0300

    front de listagem de serie personalizado de acordo com o login do usuario

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -2,22 +2,27 @@
     <ul class="list-group custom-list">
         @foreach ($series as $serie)
             <li class="list-group-item d-flex justify-content-between align-item-center">
-                <a href="{{ route('seasons.index', $serie->id) }}">
+                @auth
+                    <a href="{{ route('seasons.index', $serie->id) }}">
+                @endauth
                     {{ $serie->nome}}
-                </a>
-
-                <span class="d-flex">
-                    <a href="{{ route('series.edit', $serie->id) }}" class="btn btn-primary btn-sm">
-                        EDIT
+                @auth
                     </a>
+                @endauth
 
-                    <form action="{{ route('series.destroy', $serie->id) }}" method="post" class="ms-2">
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-danger btn-sm"> X </button>
-                    </form>
-                </span>
+                @auth
+                    <span class="d-flex">
+                        <a href="{{ route('series.edit', $serie->id) }}" class="btn btn-primary btn-sm">
+                            EDIT
+                        </a>
 
+                        <form action="{{ route('series.destroy', $serie->id) }}" method="post" class="ms-2">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-danger btn-sm"> X </button>
+                        </form>
+                    </span>
+                @endauth
             </li>
         @endforeach
     </ul>
