30d3765d26b9b65ad83fe9aedfa7f390c1bc3376
Gustavo_Vinicius_SX <gus.tec.dev@gmail.com>
2024-05-06 22:30:12 -0300

    configuração inicial de listener e event

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Event;
+
+use App\Listeners\EmailUsersAboutSeriesCreated;
+use App\Events\SeriesCreated;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Event::listen(
+            SeriesCreated::class,
+            EmailUsersAboutSeriesCreated::class,
+        );
     }
 }
