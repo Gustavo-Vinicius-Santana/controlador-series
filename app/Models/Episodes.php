7fc6a7e54d15f22eb7487efffc29a3a24d9facf8
Gustavo_Vinicius_SX <gus.tec.dev@gmail.com>
2024-05-10 19:51:38 -0300

    tratamento do watched na model episodes para que mostre true ou false no retorno da API

--- app/Models/Episodes.php
+++ app/Models/Episodes.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
@@ -15,6 +16,14 @@ class Episodes extends Model
 
     public function season(){
         return $this->belongTo(Serie::class);
+    }
+
+    protected function watched(): Attribute{
+        return new Attribute(
+            get: fn($watched) => (bool) $watched,
+            set: fn($watched) => (bool) $watched
+        );
+
     }
 
     public $timestamps = false;
