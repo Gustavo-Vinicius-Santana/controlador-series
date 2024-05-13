525d7ec7aec8ec9b1d9a25333fb4868661de460e
Gustavo_Vinicius_SX <gus.tec.dev@gmail.com>
2024-05-13 11:03:37 -0300

    verificação de usuario

--- app/Http/Controllers/Api/LoginControllerApi.php
+++ app/Http/Controllers/Api/LoginControllerApi.php
@@ -8,11 +8,20 @@ use App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Serie;
 use App\Models\Episodes;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class LoginControllerApi extends Controller
 {
     public function index(Request $request){
+        $credentials = $request->only(['email', 'password']);
 
+        $user = User::whereEmail($credentials['email'])->first();
+
+        if($user === null || Hash::check($credentials['password'], $user->password) === false){
+            return response()->json('Unauthorized', 401);
+        }
+        dd($user);
     }
 }
